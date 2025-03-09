95250570431c7ce1c295fed5e7fc2ce7fd82d133
ishwar rimal <ishwarrimal@ishwars-MacBook-Pro.local>
2025-03-09 15:57:21 +0530

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'travel-planner-wizard',
+  base: '/travel-planner-wizard/',
   server: {
     host: "::",
     port: 8080,
